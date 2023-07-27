0dfbb5d7014a038e94175e946f933e98638df104
alan-augusto <augustoalan56@gmail.com>
2023-07-26 22:47:06 -0300

    LInha do tempo

---
+++ client/src/common/CardDate/CardDate.js
@@ -0,0 +1,53 @@
+import React, { useRef, useEffect, useState } from "react";
+import "./CardDate.css";
+
+function CardDate({ title, text, image, position }) {
+  const CardDateRef = useRef(null);
+  const [CardDateHeight, setCardDateHeight] = useState(0); // Estado para armazenar a altura do CardDate
+
+  // Função para obter a altura do div CardDate e atualizar o estado CardDateHeight
+  const obterAltura = () => {
+    if (CardDateRef.current) {
+      setCardDateHeight(CardDateRef.current.offsetHeight);
+    }
+  };
+
+  // Chame obterAltura sempre que a propriedade "text" mudar para atualizar a altura, se necessário
+  useEffect(() => {
+    obterAltura();
+  }, [text]);
+
+    return (
+      <div className="CardDate" style={{ width: "75%" }} ref={CardDateRef}>
+        
+        <div className="DateContainer">
+          <div className="Date">
+            <h2>{title}</h2>
+          </div>
+          <div className="LineHistory" style={{height: CardDateHeight}}>
+          </div>
+
+        </div>
+        
+        <div className="CardDate_Text" style={{ width: "66%" }}>
+          <p>{text}</p>
+        </div>
+  
+        <img
+          src={image}
+          alt={title}
+          style={{
+            width: "34%",
+            height: CardDateHeight, // Usando a altura atualizada do CardDate como altura da imagem
+            overflow: "hidden",
+            objectFit: "cover",
+            marginLeft: "20px"
+          }}
+        />
+      </div>
+    );
+
+  
+}
+
+export default CardDate;
--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import './Home.css'
 import Card from '../../common/Card/Card';
 import Header from '../../common/Header/Header';
+import CardDate from '../../common/CardDate/CardDate';
 
 function Home() {
 
@@ -10,35 +11,48 @@ return (
     <div className='home'>
       <Header/>
       
+      <div className='Intro'>
+        <h1>Seja bem vindo à Stage</h1>
+        <hr/>
+        <br/>
+
+      </div>
+
       <Card
         title= 'Quem somos'
-        text= 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla pharetra ac tellus ut auctor. Cras at dolor dolor. Pellentesque interdum leo vel purus pellentesque commodo. Integer interdum ut odio et interdum. Cras ornare tortor et quam viverra, ac egestas tellus ultrices. Nullam at imperdiet nisi. Nullam at feugiat tellus. Cras mollis fermentum ante sit amet placerat. Praesent non iaculis orci, non fringilla mauris. Cras eget neque venenatis, ullamcorper lectus non, porttitor ipsum. Fusce in augue lacus. Sed a odio vitae ante elementum vulputate. Etiam auctor dignissim orci ac laoreet. Nulla ut est neque. Mauris vitae placerat ligula. Nullam in ipsum felis.'
+        text= 'Uma startup visionária no universo das provas competitivas automobilísticas. Nascemos a partir de uma paixão compartilhada por velocidade e desafio. Inicialmente como Ride Corporation, fundamos uma proposta inovadora de corridas por entretenimento, mas logo percebemos que poderíamos ir além. Abraçamos a ideia de proporcionar uma experiência única aos amantes do automobilismo, redefinindo o conceito de competições amadoras. Assim, a Stage emergiu como uma nova identidade, comprometida em oferecer provas emocionantes e diferenciadas, testando as habilidades dos pilotos em cenários desafiadores.'
         image= 'https://images.unsplash.com/photo-1534357808625-fdbecdd0b6da?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=735&q=80'
-        height_card={200}
-        position="left"
+        position="right"
       />
 
       <Card
         title= 'Histórico'
-        text= 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla pharetra ac tellus ut auctor. Cras at dolor dolor. Pellentesque interdum leo vel purus pellentesque commodo. Integer interdum ut odio et interdum. Cras ornare tortor et quam viverra, ac egestas tellus ultrices. Nullam at imperdiet nisi. Nullam at feugiat tellus. Cras mollis fermentum ante sit amet placerat. Praesent non iaculis orci, non fringilla mauris. Cras eget neque venenatis, ullamcorper lectus non, porttitor ipsum. Fusce in augue lacus. Sed a odio vitae ante elementum vulputate. Etiam auctor dignissim orci ac laoreet. Nulla ut est neque. Mauris vitae placerat ligula. Nullam in ipsum felis.'
+        text= 'A história da Stage é marcada por coragem e evolução. Nasceu como Ride Corporation, uma parceria audaciosa entre apaixonados por velocidade e a inovadora montadora NK. Nossa jornada começou no mundo das corridas por entretenimento, mas rapidamente percebemos o potencial para algo mais empolgante. Decidimos explorar o cenário das provas competitivas, proporcionando aos pilotos uma experiência única e revolucionária. Com uma proposta ousada, deixamos de lado o comum e criamos uma série de provas desafiadoras, indo muito além da velocidade pura. Agora, como Stage, continuamos nossa trajetória com entusiasmo, buscando constantemente redefinir o padrão das corridas amadoras e inspirar todos aqueles que têm gasolina correndo nas veias'
         image= 'https://images.unsplash.com/photo-1534357808625-fdbecdd0b6da?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=735&q=80'
-        height_card={200}
-        position="right"
+        position="left"
+      />
+      <CardDate
+        title="2013"
+        text="A NK Co. surgiu como uma montadora inovadora, focada em veículos elétricos do tipo Fórmula. Com uma visão futurista, a empresa buscava revolucionar o mercado automobilístico ao desenvolver tecnologias limpas e sustentáveis para o universo das corridas. A NK Co. rapidamente conquistou espaço como uma força disruptiva no cenário das competições, destacando-se pelo desempenho de seus veículos elétricos de alto desempenho e sua abordagem ambientalmente consciente."
+        image='https://images.unsplash.com/photo-1534357808625-fdbecdd0b6da?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=735&q=80'
       />
 
-      <Card
-        title= 'Lorem ipsum'
-        text= 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla pharetra ac tellus ut auctor. Cras at dolor dolor. Pellentesque interdum leo vel purus pellentesque commodo. Integer interdum ut odio et interdum. Cras ornare tortor et quam viverra, ac egestas tellus ultrices. Nullam at imperdiet nisi. Nullam at feugiat tellus. Cras mollis fermentum ante sit amet placerat. Praesent non iaculis orci, non fringilla mauris. Cras eget neque venenatis, ullamcorper lectus non, porttitor ipsum. Fusce in augue lacus. Sed a odio vitae ante elementum vulputate. Etiam auctor dignissim orci ac laoreet. Nulla ut est neque. Mauris vitae placerat ligula. Nullam in ipsum felis.'
-        image= 'https://images.unsplash.com/photo-1534357808625-fdbecdd0b6da?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=735&q=80'
-        height_card={200}
-        position="right"
+      <CardDate
+        title="2021"
+        text="A Ride entrou em cena como uma startup audaciosa que levou as corridas a um novo patamar. Com uma proposta única, a empresa focou em realizar corridas em ambientes exóticos, proporcionando aos competidores experiências incríveis e inesquecíveis. O formato inovador conquistou rapidamente os entusiastas do automobilismo, gerando grande visibilidade e notoriedade para a Ride como uma das startups promissoras do setor."
+        image='https://images.unsplash.com/photo-1534357808625-fdbecdd0b6da?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=735&q=80'
+        />
+
+      <CardDate
+        title="2022"
+        text="A Ride Corp. consolidou-se como uma Holding de sucesso, adquirindo participações acionárias majoritárias tanto na NK Co. quanto na Ride. Essa sinergia permitiu que as empresas trabalhassem de forma colaborativa e explorassem novas oportunidades de negócios. A Ride Corp. tornou-se uma referência no mercado, integrando a inovação tecnológica da NK Co. com a criatividade e o apelo emocional das corridas exóticas da Ride."
+        image='https://images.unsplash.com/photo-1534357808625-fdbecdd0b6da?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=735&q=80'
       />
-      <Card
-        title= 'Lorem ipsum'
-        text= 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla pharetra ac tellus ut auctor. Cras at dolor dolor. Pellentesque interdum leo vel purus pellentesque commodo. Integer interdum ut odio et interdum. Cras ornare tortor et quam viverra, ac egestas tellus ultrices. Nullam at imperdiet nisi. Nullam at feugiat tellus. Cras mollis fermentum ante sit amet placerat. Praesent non iaculis orci, non fringilla mauris. Cras eget neque venenatis, ullamcorper lectus non, porttitor ipsum. Fusce in augue lacus. Sed a odio vitae ante elementum vulputate. Etiam auctor dignissim orci ac laoreet. Nulla ut est neque. Mauris vitae placerat ligula. Nullam in ipsum felis.'
-        image= 'https://images.unsplash.com/photo-1534357808625-fdbecdd0b6da?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=735&q=80'
-        height_card={200}
-        position="left"
+      
+      <CardDate
+        title="2023"
+        text="Marcou o nascimento da Stage, a futura promotora das provas competitivas. Em busca de uma abordagem pioneira e emocionante, a Stage surgiu como uma evolução natural da Ride Corp., redefinindo completamente o conceito de competições amadoras. Com provas desafiadoras e cenários emocionantes, a Stage promete levar os pilotos a uma jornada única de adrenalina e superação. A startup se compromete a proporcionar uma experiência revolucionária no universo automobilístico, com a combinação perfeita de tecnologia, paixão por velocidade e respeito ao meio ambiente. Prepare-se para embarcar nessa jornada emocionante rumo ao futuro das corridas competitivas!"
+        image='https://images.unsplash.com/photo-1534357808625-fdbecdd0b6da?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=735&q=80'
       />
 
     </div>
