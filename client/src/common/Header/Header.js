77112f3c0c834aa2ced5484fd0d45702dd837691
alan-augusto <augustoalan56@gmail.com>
2023-07-31 22:58:01 -0300

    botão deploy

--- client/src/common/Header/Header.js
+++ client/src/common/Header/Header.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useRef, useState } from "react";
 import "./Header.css";
-import Button from "../Button/Button.js";
 import { useNavigate } from "react-router-dom";
 import SearchBar from "./SearchBar/SearchBar";
+import Button from "..//Button/Button";
 
 const useOutsideClick = (callback) => {
   const ref = useRef();
