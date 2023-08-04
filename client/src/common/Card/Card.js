ed502b3774d283da3455a437aabcae7f0ce51ee8
Caio Lelis Penido <caiolelis337@hotmail.com>
2023-08-04 17:01:44 -0300

    adicionando responsividade

--- client/src/common/Card/Card.js
+++ client/src/common/Card/Card.js
@@ -22,50 +22,27 @@ function Card({ title, text, image, position, cardHeight }) {
   // Usando a altura passada ou a altura atualizada do card como altura da imagem
   const cardImageHeight = cardHeight || currentCardHeight;
 
-  if (position === "left") {
-    return (
-      <div className="Card" ref={cardRef}>
-        {image && (
-          <img
-            src={image}
-            alt={title}
-            style={{
-              height: cardImageHeight,
-              overflow: "hidden",
-              objectFit: "cover",
-              marginRight: "20px",
-            }}
-          />
-        )}
+  return (
+    <div className="Card" ref={cardRef}>
+      {image && (
+        <img
+          src={image}
+          alt={title}
+          style={{
+            height: cardImageHeight,
+            overflow: "hidden",
+            objectFit: "cover",
+            marginRight: "20px",
+          }}
+        />
+      )}
 
-        <div className="Card_Text">
-          <h2>{title}</h2>
-          <p>{text}</p>
-        </div>
+      <div className="Card_Text">
+        <h2>{title}</h2>
+        <p>{text}</p>
       </div>
-    );
-  } else if (position === "right") {
-    return (
-      <div className="Card" ref={cardRef}>
-        <div className="Card_Text">
-          <h2>{title}</h2>
-          <p>{text}</p>
-        </div>
-        {image && (
-          <img
-            src={image}
-            alt={title}
-            style={{
-              height: cardImageHeight,
-              overflow: "hidden",
-              objectFit: "cover",
-              marginLeft: "20px",
-            }}
-          />
-        )}
-      </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default Card;
