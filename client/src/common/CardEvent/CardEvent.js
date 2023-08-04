6d83c0edce7ad485d3123a045f5ff5141a6cecdc
Caio Lelis Penido <caiolelis337@hotmail.com>
2023-08-04 16:07:23 -0300

    adicionando responsividade

--- client/src/common/CardEvent/CardEvent.js
+++ client/src/common/CardEvent/CardEvent.js
@@ -19,40 +19,37 @@ function CardEvent({ title, date1, date2, local, image }) {
 
   return (
     <div className="CardEvent" style={{ width: "75%" }} ref={CardEventRef}>
-      
       <img
         src={image}
         alt={title}
         style={{
-          width: "34%",
           height: CardEventHeight, // Usando a altura atualizada do CardEvent como altura da imagem
           overflow: "hidden",
           objectFit: "cover",
-          marginRight: "20px"
+          marginRight: "20px",
         }}
       />
 
-      <div className="CardEvent_Text" style={{ width: "66%" }}>
+      <div className="CardEvent_Text">
         <h2>{title}</h2>
         <div className="Event-element">
-          <img src={require('./assets/calendar.png')}/>
+          <img id="icons" src={require("./assets/calendar.png")} />
           <p>{date1}</p>
         </div>
-        {date2 && date2.trim() !== "" && ( // Verifica se date2 existe e não é uma string vazia
-          <div className="Event-element">
-            <img src={require('./assets/calendar.png')}/>
-            <p>{date2}</p>
-          </div>
-        )}
+        {date2 &&
+          date2.trim() !== "" && ( // Verifica se date2 existe e não é uma string vazia
+            <div className="Event-element">
+              <img id="icons" src={require("./assets/calendar.png")} />
+              <p>{date2}</p>
+            </div>
+          )}
         <div className="Event-element">
-          <img src={require('./assets/location.png')}/>
+          <img id="icons" src={require("./assets/location.png")} />
           <p>{local}</p>
         </div>
       </div>
-
     </div>
   );
-  
 }
 
 export default CardEvent;
