c84cbc5a2a4efcb8efaf7a53d352e7a42054e8a4
Caio Lelis Penido <caiolelis337@hotmail.com>
2023-08-05 10:33:59 -0300

    adicionando responsividade

--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -1,35 +1,35 @@
-import {Route, Routes, BrowserRouter as Router} from 'react-router-dom'
-import Home from '../pages/home/Home'
-import Provas from '../pages/provas/Provas'
-import Categorias from '../pages/categorias/Categorias'
-import Parceiros from '../pages/parceiros/Parceiros'
-import Contatos from '../pages/contatos/Contatos'
-import Equipe from '../pages/equipe/Equipe'
-import Inscrever from '../pages/inscrição/Inscrever'
-import Historia from '../pages/historia/Historia'
+import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
+import Home from "../pages/home/Home";
+import Provas from "../pages/provas/Provas";
+import Categorias from "../pages/categorias/Categorias";
+import Parceiros from "../pages/parceiros/Parceiros";
+import Contatos from "../pages/contatos/Contatos";
+import Equipe from "../pages/equipe/Equipe";
+import Inscrever from "../pages/inscrição/Inscrever";
+import Historia from "../pages/historia/Historia";
 
-import SP24 from '../pages/temporadas/SP24/SP24'
-import SP25 from '../pages/temporadas/SP25/SP25'
-import MG24 from '../pages/temporadas/MG24/MG24'
-import PB25 from '../pages/temporadas/PB25/PB25'
+import SP24 from "../pages/temporadas/SP24/SP24";
+import SP25 from "../pages/temporadas/SP25/SP25";
+import MG24 from "../pages/temporadas/MG24/MG24";
+import PB25 from "../pages/temporadas/PB25/PB25";
 
-export const RouterApp=()=>{
-    return(
-        <Router>
-            <Routes>
-                <Route path='/' element={<Home/>}/>
-                <Route path='/nossahistoria' element={<Historia/>}/>
-                <Route path='/provas' element={<Provas/>}/>
-                <Route path='/categorias' element={<Categorias/>}/>
-                <Route path='/parceiros' element={<Parceiros/>}/>
-                <Route path='/contatos' element={<Contatos/>}/>
-                <Route path='/equipe' element={<Equipe/>}/>
-                <Route path='temporadas/SP24' element={<SP24/>}/>
-                <Route path='temporadas/SP25' element={<SP25/>}/>
-                <Route path='temporadas/MG24' element={<MG24/>}/>
-                <Route path='temporadas/PB25' element={<PB25/>}/>
-                <Route path='/inscrição' element={<Inscrever/>}/>
-            </Routes>
-        </Router>
-    )
-}
+export const RouterApp = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Historia />} />
+        <Route path="/nossahistoria" element={<Historia />} />
+        <Route path="/provas" element={<Provas />} />
+        <Route path="/categorias" element={<Categorias />} />
+        <Route path="/parceiros" element={<Parceiros />} />
+        <Route path="/contatos" element={<Contatos />} />
+        <Route path="/equipe" element={<Equipe />} />
+        <Route path="temporadas/SP24" element={<SP24 />} />
+        <Route path="temporadas/SP25" element={<SP25 />} />
+        <Route path="temporadas/MG24" element={<MG24 />} />
+        <Route path="temporadas/PB25" element={<PB25 />} />
+        <Route path="/inscrição" element={<Inscrever />} />
+      </Routes>
+    </Router>
+  );
+};
